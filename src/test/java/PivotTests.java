fe07e2c2b78e54bea66b202652168c832959500d
Kevin Reas <76408202+PaarkG@users.noreply.github.com>
2025-03-01 23:06:02 -0500

    more tests

--- src/test/java/PivotTests.java
+++ src/test/java/PivotTests.java
@@ -9,13 +9,50 @@ import org.junit.jupiter.api.Test;
 
 class PivotTests {
     @Test
-    void safePositionAlgorithmWhenSafe() {
+    void l4SafePosition() {
         assertEquals(true, PivotSubsystem.isRobotPositionSafe(PivotPosition.L_FOUR));
     }
 
     @Test
-    void safePositionAlgorithmWhenUnsafe() {
+    void l3SafePosition() {
+        assertEquals(true, PivotSubsystem.isRobotPositionSafe(PivotPosition.L_THREE));
+    }
+
+    @Test
+    void l2SafePosition() {
+        assertEquals(true, PivotSubsystem.isRobotPositionSafe(PivotPosition.L_TWO));
+    }
+
+    @Test
+    void l1SafePosition() {
+        assertEquals(true, PivotSubsystem.isRobotPositionSafe(PivotPosition.L_ONE));
+    }
+
+    @Test
+    void primeSafePosition() {
+        assertEquals(true, PivotSubsystem.isRobotPositionSafe(PivotPosition.PRIME));
+    }
+
+
+    @Test
+    void coralStationSafePosition() {
+        assertEquals(true, PivotSubsystem.isRobotPositionSafe(PivotPosition.CORAL_STATION_INTAKE));
+    }
+
+
+    @Test
+    void storedSafePosition() {
+        assertEquals(false, PivotSubsystem.isRobotPositionSafe(PivotPosition.STORED));
+    }
+
+    @Test
+    void groundIntakeSafePosition() {
         assertEquals(false, PivotSubsystem.isRobotPositionSafe(PivotPosition.GROUND_INTAKE));
+    }
+
+    @Test
+    void safePositionSafePosition() {
+        assertEquals(true, PivotSubsystem.isRobotPositionSafe(PivotPosition.SAFE_POSITION));
     }
 }
 
