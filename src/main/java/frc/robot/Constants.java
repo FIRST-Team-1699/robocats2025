89bf18d7e334508bfa69b785e0f9cd84de66aef3
Kevin Reas <76408202+PaarkG@users.noreply.github.com>
2025-04-30 12:51:52 +0000

    Documented servo class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,11 +24,10 @@ public class Constants {
     }
   
     public static class AutoConstants {
-        // TRANSLATION PID
         public static final double kTranslationP = 10;
         public static final double kTranslationI = 0;
         public static final double kTranslationD = 0;
-        // ROTATION PID
+        
         public static final double kRotationP = 7;
         public static final double kRotationI = 0;
         public static final double kRotationD = 0;
